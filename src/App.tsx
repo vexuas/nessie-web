7d3c339206cd3946cce3f6641b1d7a59b79d5903
Gabe R <rodriguezgab1601@gmail.com>
2022-06-15 00:01:41 +0800

    Add terms route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import WorkInProgress from './app/components/WorkInProgress';
 
 function App() {
   return (
     <div className="App">
-      <WorkInProgress />
+      <Routes>
+        <Route path="/" element={<WorkInProgress />} />
+        <Route path="/terms" element={<div>hello</div>} />
+      </Routes>
     </div>
   );
 }
