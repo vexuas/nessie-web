81a8fc2997ed51f8a9f45313b207aad368e879b0
Gabe R <rodriguezgab1601@gmail.com>
2022-06-16 01:02:55 +0800

    Create footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import WorkInProgress from './app/components/WorkInProgress';
 import TermsContainer from './app/components/TermsContainer';
+import Footer from './app/components/Footer';
 import styles from './App.module.scss';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<WorkInProgress />} />
         <Route path="/terms" element={<TermsContainer />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import styles from './Footer.module.scss';
+
+export default function Footer() {
+  return (
+    <div className={styles.Container}>
+      <div className={styles.Contact}>hello</div>
+      <div className={styles.Links}></div>
+    </div>
+  );
+}
