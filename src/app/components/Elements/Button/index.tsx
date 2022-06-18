7dc2b3955cd593c1bf0d65f6c73b3fdf595525ce
Gabe R <rodriguezgab1601@gmail.com>
2022-06-19 00:22:02 +0800

    Add invite button to nav

--- src/app/components/Elements/Button/index.tsx
+++ src/app/components/Elements/Button/index.tsx
@@ -3,11 +3,12 @@ import styles from './Button.module.scss';
 interface Props {
   className?: string;
   label: string;
+  onClick?: () => void;
 }
 
-export default function Button({ className, label }: Props) {
+export default function Button({ className, label, onClick }: Props) {
   return (
-    <button className={`${styles.Container} ${className}`}>
+    <button className={`${styles.Container} ${className}`} onClick={onClick}>
       <span>{label}</span>
     </button>
   );
