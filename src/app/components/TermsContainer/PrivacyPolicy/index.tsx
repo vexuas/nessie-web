2899856a69484ce6aa85d38b8594ac3f6d073a11
vexuas <rodriguezgab1601@gmail.com>
2025-06-30 01:24:58 +0800

    Fix dom validation

--- src/app/components/TermsContainer/PrivacyPolicy/index.tsx
+++ src/app/components/TermsContainer/PrivacyPolicy/index.tsx
@@ -5,21 +5,20 @@ export default function PrivacyPolicy() {
     <div className={styles.Section} data-testid="PrivacyPolicy" id="privacy_policy">
       <div className={styles.Title}>Privacy Policy</div>
       <div className={styles.Description}>
-        <p>
+        <div>
           To deliver the best experience for our users, the following information is stored solely
           for their proper operation:
-          <ul>
-            <li>
-              Discord Server identifiers to associate your servers of configuration within the
-              Service;
-            </li>
-            <li>
-              Interactions you have made with the Bot, including commands executed, time of
-              execution, Discord server, channel and user of execution, and any arguments passed to
-              the command.
-            </li>
-          </ul>
-        </p>
+        </div>
+        <ul>
+          <li>
+            Discord Server identifiers to associate your servers of configuration within the
+            Service;
+          </li>
+          <li>
+            Interactions you have made with the Bot, including commands executed, time of execution,
+            Discord server, channel and user of execution, and any arguments passed to the command.
+          </li>
+        </ul>
         <p>
           We retain the collected information as long as is necessary. Data is removed when the
           reason for its collection is no longer applicable.
