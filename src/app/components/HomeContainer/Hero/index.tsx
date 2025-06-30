f46bd19ae76831bd6d38f81b1125eebf8907cf53
vexuas <rodriguezgab1601@gmail.com>
2025-06-30 21:11:24 +0800

    Create base layout for Hero component

---
+++ src/app/components/HomeContainer/Hero/index.tsx
@@ -0,0 +1,23 @@
+import Button from '../../Elements/Button';
+
+export default function Hero() {
+  return (
+    <div>
+      <div>
+        <div>Stay up-to-date with Apex Legends Map Rotations </div>
+        <div>
+          Tired of not knowing what maps can be played? Ever booted up the game only to find out
+          your least favourite map is on? (king’s canyon bleghh) Suffer no more, with Nessie you
+          will always know what map is in rotation, all in the comforts of your own Discord server
+        </div>
+        <div>
+          <Button label="Add to discord" />
+          <Button label="See features" />
+        </div>
+      </div>
+      <div>
+        <img src="https://cdn.vexuas.com/Wallpapers/maki_full.jpg" />
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/HomeContainer/index.tsx
@@ -0,0 +1,9 @@
+import Hero from './Hero';
+
+export default function HomeContainer() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
