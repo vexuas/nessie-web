0ead267af7262e0431b61af98fd9f8661b827cd5
vexuas <rodriguezgab1601@gmail.com>
2025-07-01 03:12:19 +0800

    Add layout and styles

--- src/app/components/HomeContainer/Logos/index.tsx
+++ src/app/components/HomeContainer/Logos/index.tsx
@@ -1,3 +1,22 @@
+import styles from './Logos.module.scss';
+
 export default function Logos() {
-  return <div>Logos here</div>;
+  const MOCK_LOGOS: string[] = [
+    'https://cdn.vexuas.com/Wallpapers/you_cute.jpg',
+    'https://cdn.vexuas.com/Wallpapers/you_cute.jpg',
+    'https://cdn.vexuas.com/Wallpapers/you_cute.jpg',
+    'https://cdn.vexuas.com/Wallpapers/you_cute.jpg',
+    'https://cdn.vexuas.com/Wallpapers/you_cute.jpg',
+  ];
+
+  const t_logos = MOCK_LOGOS.map((logo, index) => {
+    return <img className={styles.Logo} key={index} src={logo} alt="logo" />;
+  });
+
+  return (
+    <div className={styles.Container}>
+      <div className={styles.Title}>Join 123456 users in 2000 servers using Nessie</div>
+      <div className={styles.Logos}>{t_logos}</div>
+    </div>
+  );
 }
