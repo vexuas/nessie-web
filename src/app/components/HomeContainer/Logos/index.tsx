344d679d8325d393da1ab7fd21f7f2be622a3965
vexuas <rodriguezgab1601@gmail.com>
2025-07-02 00:34:59 +0800

    Add autoplay

--- src/app/components/HomeContainer/Logos/index.tsx
+++ src/app/components/HomeContainer/Logos/index.tsx
@@ -1,6 +1,8 @@
 import styles from './Logos.module.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/autoplay';
 
 // TODO: Upload server logos to cdn and hardcode them here
 // TODO: Add carousel functionality
@@ -31,7 +33,15 @@ export default function Logos() {
   return (
     <div className={styles.Container} data-testid="Logos">
       <div className={styles.Title}>Join 123456 users in 2000 servers using Nessie</div>
-      <Swiper className={styles.Logos} slidesPerView={6}>
+      <Swiper
+        freeMode
+        loop
+        autoplay={{ delay: 0 }}
+        modules={[Autoplay]}
+        className={styles.Logos}
+        slidesPerView={6}
+        speed={5000}
+      >
         {t_logos}
       </Swiper>
     </div>
