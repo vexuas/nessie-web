b23033deceea10c3623435fd519ad1252c285bf0
vexuas <rodriguezgab1601@gmail.com>
2025-07-03 00:52:25 +0800

    Use useMemo for logos

--- src/app/components/HomeContainer/index.tsx
+++ src/app/components/HomeContainer/index.tsx
@@ -1,9 +1,13 @@
+import { useMemo } from 'react';
 import { SERVER_LOGOS } from '../../../utils/constants';
 import Hero from './Hero';
 import Logos from './Logos';
 
 export default function HomeContainer() {
-  const logos: { name: string; src: string }[] = [...SERVER_LOGOS].sort(() => Math.random() - 0.5); // Shuffle the position of logos on every render for fun
+  const logos: { name: string; src: string }[] = useMemo(() => {
+    return [...SERVER_LOGOS].sort(() => Math.random() - 0.5); // Shuffle the position of logos for fun
+  }, [SERVER_LOGOS]);
+
   const noOfUsers: string = '130,000';
   const noOfServers: string = '1400';
 
