98405cd19e35adc7ead6a5b4d81c91aedab9ca81
vexuas <rodriguezgab1601@gmail.com>
2025-07-02 20:17:33 +0800

    Add actual server logos

--- src/app/components/HomeContainer/index.tsx
+++ src/app/components/HomeContainer/index.tsx
@@ -1,28 +1,15 @@
+import { SERVER_LOGOS } from '../../../utils/constants';
 import Hero from './Hero';
 import Logos from './Logos';
 
 export default function HomeContainer() {
-  const MOCK_LOGOS: string[] = [
-    'https://cdn.vexuas.com/Wallpapers/you_cute.jpg',
-    'https://cdn.vexuas.com/Wallpapers/maki_full.jpg',
-    'https://cdn.vexuas.com/Wallpapers/you_cute.jpg',
-    'https://cdn.vexuas.com/Wallpapers/maki_full.jpg',
-    'https://cdn.vexuas.com/Wallpapers/you_cute.jpg',
-    'https://cdn.vexuas.com/Wallpapers/maki_full.jpg',
-    'https://cdn.vexuas.com/Wallpapers/you_cute.jpg',
-    'https://cdn.vexuas.com/Wallpapers/maki_full.jpg',
-    'https://cdn.vexuas.com/Wallpapers/you_cute.jpg',
-    'https://cdn.vexuas.com/Wallpapers/maki_full.jpg',
-    'https://cdn.vexuas.com/Wallpapers/you_cute.jpg',
-    'https://cdn.vexuas.com/Wallpapers/maki_full.jpg',
-  ];
   const noOfUsers = '130,000';
   const noOfServers = '1400';
 
   return (
     <div data-testid="HomeContainer">
       <Hero />
-      <Logos logos={MOCK_LOGOS} noOfUsers={noOfUsers} noOfServers={noOfServers} />
+      <Logos logos={SERVER_LOGOS} noOfUsers={noOfUsers} noOfServers={noOfServers} />
     </div>
   );
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,17 @@
 export const DISCORD_INVITE_LINK =
   'https://discord.com/api/oauth2/authorize?client_id=889135055430111252&permissions=536874000&scope=applications.commands%20bot';
+
+export const SERVER_LOGOS = [
+  'https://cdn.vexuas.com/nessie/servers/%E1%84%80%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%B7%E1%84%8F%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A5%E1%86%B8.webp',
+  'https://cdn.vexuas.com/nessie/servers/%E1%84%8B%E1%85%A6%E1%84%91%E1%85%A6%E1%86%A8%E1%84%87%E1%85%A1%E1%86%BC.webp',
+  'https://cdn.vexuas.com/nessie/servers/dokuwaki.webp',
+  'https://cdn.vexuas.com/nessie/servers/%E1%84%80%E1%85%A7%E1%86%BC%E1%84%85%E1%85%A9%E1%84%83%E1%85%A1%E1%86%BC.webp',
+  'https://cdn.vexuas.com/nessie/servers/%E1%84%88%E1%85%A1%E1%86%AB%E1%84%89%E1%85%B3%E1%84%80%E1%85%A9%E1%86%AF%E1%84%85%E1%85%A6%E1%86%B7.gif',
+  'https://cdn.vexuas.com/nessie/servers/Apex_Vanguard.webp',
+  'https://cdn.vexuas.com/nessie/servers/the_fire_nation.webp',
+  'https://cdn.vexuas.com/nessie/servers/%E1%84%87%E1%85%B3%E1%84%87%E1%85%AE%E1%86%BC%E1%84%80%E1%85%B3%E1%86%AF%E1%84%8B%E1%85%A9%E1%86%B8%E1%84%89%E1%85%A5%E1%84%87%E1%85%A5%20.gif',
+  'https://cdn.vexuas.com/nessie/servers/Discord.webp',
+  'https://cdn.vexuas.com/nessie/servers/Game_Lounge.webp',
+  'https://cdn.vexuas.com/nessie/servers/Lockers.webp',
+  'https://cdn.vexuas.com/nessie/servers/Black_Jack.webp',
+];
