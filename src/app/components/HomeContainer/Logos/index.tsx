2a3af38f56a04157a0367facce73ab5d6d7d1342
vexuas <rodriguezgab1601@gmail.com>
2025-07-02 22:04:26 +0800

    Add tests

--- src/app/components/HomeContainer/Logos/index.tsx
+++ src/app/components/HomeContainer/Logos/index.tsx
@@ -28,7 +28,7 @@ export default function Logos({ logos, noOfUsers, noOfServers }: Props) {
   const t_logos = logos.map((logo, index) => {
     return (
       <SwiperSlide key={index}>
-        <img className={styles.Logo} data-testid="Logo" src={logo.src} alt={logo.name} />
+        <img className={styles.Logo} data-testid="Logos__logo" src={logo.src} alt={logo.name} />
       </SwiperSlide>
     );
   });
@@ -36,8 +36,15 @@ export default function Logos({ logos, noOfUsers, noOfServers }: Props) {
   return (
     <div className={styles.Container} data-testid="Logos">
       <div className={styles.Title}>
-        Join over <span className={styles.Users}>{noOfUsers}</span> users in{' '}
-        <span className={styles.Servers}>{noOfServers}+</span> servers using Nessie
+        Join over{' '}
+        <span className={styles.Users} data-testid="Logos__users">
+          {noOfUsers}
+        </span>{' '}
+        users in{' '}
+        <span className={styles.Servers} data-testid="Logos__servers">
+          {noOfServers}+
+        </span>{' '}
+        servers using Nessie
       </div>
       <Swiper
         freeMode
