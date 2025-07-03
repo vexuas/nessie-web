b51feade3bf673e04717f713e816b62bf670b8f6
vexuas <rodriguezgab1601@gmail.com>
2025-07-04 01:27:54 +0800

    Create Banner component

---
+++ src/app/components/HomeContainer/Banner/index.tsx
@@ -0,0 +1,10 @@
+import Button from '../../Elements/Button';
+
+export default function Banner() {
+  return (
+    <div>
+      <div>Always stay up-to-date</div>
+      <Button label="Invite" />
+    </div>
+  );
+}
