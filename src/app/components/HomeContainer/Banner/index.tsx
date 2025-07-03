59c7c8f7ee383477adbb9a83ad9c94eafabbb1d1
vexuas <rodriguezgab1601@gmail.com>
2025-07-04 02:10:47 +0800

    Add tests

--- src/app/components/HomeContainer/Banner/index.tsx
+++ src/app/components/HomeContainer/Banner/index.tsx
@@ -4,7 +4,7 @@ import styles from './Banner.module.scss';
 
 export default function Banner() {
   return (
-    <div className={styles.Container}>
+    <div className={styles.Container} data-testid="Banner">
       <div className={styles.Banner}>
         <div className={styles.Title}>Always stay up-to-date</div>
         <a href={DISCORD_INVITE_LINK} target="_blank" rel="noreferrer">
