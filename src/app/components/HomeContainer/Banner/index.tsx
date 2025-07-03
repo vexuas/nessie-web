9a599d4bc08aa3957a38216a533b7a24bacc4def
vexuas <rodriguezgab1601@gmail.com>
2025-07-04 01:42:25 +0800

    Add styles

--- src/app/components/HomeContainer/Banner/index.tsx
+++ src/app/components/HomeContainer/Banner/index.tsx
@@ -1,10 +1,13 @@
 import Button from '../../Elements/Button';
+import styles from './Banner.module.scss';
 
 export default function Banner() {
   return (
-    <div>
-      <div>Always stay up-to-date</div>
-      <Button label="Invite" />
+    <div className={styles.Container}>
+      <div className={styles.Banner}>
+        <div className={styles.Title}>Always stay up-to-date</div>
+        <Button className={styles.Button} label="Invite" />
+      </div>
     </div>
   );
 }
